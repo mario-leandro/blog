cf413dc75fb16d4ef472c397cf7554111fb74138
mario-leandro <marioleandro2004@gmail.com>
2025-02-12 11:46:02 -0300

    Mudando projeto do javascript para typescript

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import { Inter } from "next/font/google";
+import "@/styles/globals.sass";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Blog",
+  description: "Projeto de blog com Next.js e Sass",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
